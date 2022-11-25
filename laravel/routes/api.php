f75f33dd8cfe9771cd1fa2f286782c199b016446
RetmixX <jameslayng123@gmail.com>
2022-11-25 11:55:27 +0700

    a

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("main", "App\Http\Controllers\Head@head");
+
+Route::fallback(function (){
+   abort(404, "А?");
 });
 
 
